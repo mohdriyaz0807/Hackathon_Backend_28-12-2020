228a7bd30abc3c83abe14bd326ed8927ba231577
Mohamed Riyaz <mohdriyaz0807@gmail.com>
2021-02-09 08:49:54 +0530

    cors

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ const newObjectId = mongodb.ObjectID()
 const app = express();
 const dbURL = process.env.DB_URL ||"mongodb://127.0.0.1:27017";
 const port = process.env.PORT || 4000
-app.use(cors())
 app.use(express.json());
+app.use(cors())
 
 const instance = new Razorpay({
   key_id: process.env.RAZOR_PAY_KEY_ID,
