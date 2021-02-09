d678e984be9afa7572f2982ded1ebcf1fa21fcb4
Mohamed Riyaz <mohdriyaz0807@gmail.com>
2021-02-09 10:19:45 +0530

    cors

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const dbURL = process.env.DB_URL ||"mongodb://127.0.0.1:27017";
 const port = process.env.PORT || 4000
 // app.use(express.json());
 app.use(function (req,res,next){
-  res.header("Access-Control-Allow-Origin","*");
+  res.header("Access-Control-Allow-Origin","http://localhost:3000");
   res.header("Access-Control-Allow-Headers","Origin,X-Requeted-With,Content-Type,Accept");
   res.header("Access-Control-Allow-Methods","GET,POST,OPTIONS,PUT,DELETE");
   res.header("Access-Control-Allow-Credentials",true)
